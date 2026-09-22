b3317ee1c5f74c69f7ac2cbd56318397b04ada21
DePoly877 <denys.korolevych.ri.2026@lpnu.ua>
2026-09-22 23:36:38 +0300

    Update code: comment z2 calculation

--- 2.0.cpp
+++ 2.0.cpp
@@ -4,14 +4,14 @@ int main()
 {
 	double x; // вхідний параметр
 	double z1; // результат обчислення 1-го виразу
-	double z2; // результат обчислення 2-го виразу
+	// double z2; // результат обчислення 2-го виразу
 	cout << "x = "; cin >> x;
 	z1 = (x * x + 2 * x - 3 + (x + 1) * sqrt(x * x - 9)) /
 		(x * x - 2 * x - 3 + (x - 1) * sqrt(x * x - 9));
-	z2 = sqrt((x + 3) / (x - 3));
+	// z2 = sqrt((x + 3) / (x - 3));
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
-	cout << "z2 = " << z2 << endl;
+	// cout << "z2 = " << z2 << endl;
 	cin.get();
 	return 0;
 }
